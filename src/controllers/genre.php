d3ec78cc18f5734379c4806052f5c65b090cc11f
Justin Nguyen <justin.nguyen03@sjsu.edu>
2020-10-30 17:41:13 -0700

    fixed

--- src/controllers/genre.php
+++ src/controllers/genre.php
@@ -2,11 +2,13 @@
   $path = $_SERVER['DOCUMENT_ROOT'];
   include_once($path.'/CS174-hw3/src/models/storedb.php');
   include_once($path.'/CS174-hw3/src/views/landingView.php');
-  
+
   $genre = $_REQUEST['genre'];
 
   $call_view = new landingView();
   $call_db = new manageDB();
   $call_db->insertGenre($genre);
   $gArray = $call_db->fetchGenres();
-  $call_view ->  render($gArray, $gArray);
+  $rArray = $call_db->fetchReviewsTitle();
+  $dArray = $call_db->fetchReviewsDate();
+  $call_view ->render($gArray, $rArray,$dArray);
